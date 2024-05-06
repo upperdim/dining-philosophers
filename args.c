4a596bba0657c296caea4d8df703bed52145ddb2
upperdim <45048352+upperdim@users.noreply.github.com>
2024-05-06 17:56:47 +0200

    added some todos for edge cases

--- args.c
+++ args.c
@@ -6,11 +6,11 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 12:47:20 by tunsal            #+#    #+#             */
-/*   Updated: 2024/04/04 16:10:31 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/05/06 17:40:29 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../philo.h"
+#include "philo.h"
 
 void	init_args(t_sim *sim)
 {
@@ -19,6 +19,7 @@ void	init_args(t_sim *sim)
 
 void	parse_args(int argc, char *argv[], t_sim *sim)
 {
+	// TODO: atoi() / atol()
 	sim->num_of_philos = atoi(argv[1]);
 	sim->time_to_die_wo_eating = atoi(argv[2]);
 	sim->time_to_eat = atoi(argv[3]);
@@ -27,11 +28,15 @@ void	parse_args(int argc, char *argv[], t_sim *sim)
 		sim->meal_limit = atoi(argv[5]);
 	else
 		sim->meal_limit = NO_LIMIT;
+	// TODO: Handle if max number of meals is 0
+	// TODO: Handle if only 1 philo
 }
 
 int	validate_args(int argc, char *argv[])
 {
-		if (argc != 5 || argc != 6)
+	// TODO: str_is_numeric()
+	// TODO: check for INT_MAX
+	if (argc != 5 || argc != 6)
 		return (EXIT_FAILURE);
 	if (!str_is_int(argv[1]) || !str_is_numeric(argv[2]) 
 		|| !str_is_numeric(argv[3]) || !str_is_numeric(argv[4])
