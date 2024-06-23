38f8ed5b62bcd21c67fd1238d0b9f68b7b1bdb56
upperdim <45048352+upperdim@users.noreply.github.com>
2024-06-23 19:03:30 +0200

    moved and cleaned unused getters and setters

--- src/threads/getters_setters.c
+++ src/threads/getters_setters.c
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 22:43:26 by tunsal            #+#    #+#             */
-/*   Updated: 2024/05/06 22:49:29 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/06/23 18:26:54 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 /* Set value `val` to integer `dest` which is protected by `mutex` */
 void	set_int(pthread_mutex_t *mutex, int *dest, int val)
 {
-	safe_mutex_handle(mutex, LOCK);
+	pthread_mutex_lock(mutex);
 	*dest = val;
-	safe_mutex_handle(mutex, UNLOCK);
+	pthread_mutex_unlock(mutex);
 }
 
 /* Get value of integer `dest` which is protected by `mutex` */
@@ -25,27 +25,8 @@ int	get_int(pthread_mutex_t *mutex, int *dest)
 {
 	int	ret;
 
-	safe_mutex_handle(mutex, LOCK);
+	pthread_mutex_lock(mutex);
 	ret = *dest;
-	safe_mutex_handle(mutex, UNLOCK);
-	return (ret);
-}
-
-/* Set value `val` to long `dest` which is protected by `mutex` */
-void	set_long(pthread_mutex_t *mutex, long *dest, int val)
-{
-	safe_mutex_handle(mutex, LOCK);
-	*dest = val;
-	safe_mutex_handle(mutex, UNLOCK);
-}
-
-/* Get value of integer `dest` which is protected by `mutex` */
-int	get_long(pthread_mutex_t *mutex, long *dest)
-{
-	long	ret;
-
-	safe_mutex_handle(mutex, LOCK);
-	ret = *dest;
-	safe_mutex_handle(mutex, UNLOCK);
+	pthread_mutex_unlock(mutex);
 	return (ret);
 }
