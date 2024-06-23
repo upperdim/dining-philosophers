f0a30393c06a973828461a1c08275207506545b1
upperdim <45048352+upperdim@users.noreply.github.com>
2024-06-23 19:03:03 +0200

    thread sync utils

--- src/threads/sync.c
+++ src/threads/sync.c
@@ -1,28 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   thread_sync.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/06 17:55:24 by tunsal            #+#    #+#             */
-/*   Updated: 2024/05/07 17:30:23 by tunsal           ###   ########.fr       */
+/*   Created: 2024/06/23 17:32:51 by tunsal            #+#    #+#             */
+/*   Updated: 2024/06/23 17:34:05 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	error_exit(const char *err_msg)
-{
-	if (err_msg != NULL)
-		printf("%s\n", err_msg);
-	exit(EXIT_FAILURE);
-}
-
 void	wait_all_threads(t_sim *sim)
 {
-	while (!get_int(&sim->sim_mutex, sim->is_all_threads_ready))
+	while (!get_int(&sim->sim_mutex, &sim->is_all_threads_ready))
 	{
 		;
 	}
+}
+
+int	is_sim_finished(t_sim *sim)
+{
+	return get_int(&sim->sim_mutex, &sim->finished);	
 }
