2028b0135108bc9e844ae39c10856e436758719d
upperdim <45048352+upperdim@users.noreply.github.com>
2024-06-24 15:48:25 +0200

    move philo actions to other functions

--- src/threads/philo_actions.c
+++ src/threads/philo_actions.c
@@ -6,14 +6,35 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 18:53:10 by tunsal            #+#    #+#             */
-/*   Updated: 2024/06/23 18:53:49 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/06/24 15:43:16 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-// eat
+void	philo_eat(t_routine_arg *arg)
+{
+	pthread_mutex_lock(&arg->forks[arg->philo->index]);
+	pthread_mutex_lock(&arg->forks[(arg->philo->index + 1) % arg->fork_count]);
+	arg->philo->state = STATE_EATING;
+	print_msg("is eating", arg->sim, arg->philo);
+	arg->philo->num_times_ate++;
+	if (gettimeofday(&arg->philo->last_eat_timestamp, NULL) == -1)
+		printf("Gettimeofday failed\n");
+	sleep_ms(arg->sim->time_to_eat, arg->sim);
+	pthread_mutex_unlock(&arg->forks[arg->philo->index]);
+	pthread_mutex_unlock(&arg->forks[(arg->philo->index + 1) % arg->fork_count]);
+}
 
-// think
+void	philo_think(t_routine_arg	*arg)
+{
+	arg->philo->state = STATE_THINKING;
+	print_msg("is thinking", arg->sim, arg->philo);
+}
 
-// sleep
+void	philo_sleep(t_routine_arg	*arg)
+{
+	arg->philo->state = STATE_SLEEPING;
+	print_msg("is sleeping", arg->sim, arg->philo);
+	sleep_ms(arg->sim->time_to_sleep, arg->sim);
+}
