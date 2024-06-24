b1c4959381f7669adb81f23a2133f3fd070fd2fd
upperdim <45048352+upperdim@users.noreply.github.com>
2024-06-24 17:28:58 +0200

    join observer thread last cause idk

--- src/threads/join.c
+++ src/threads/join.c
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 18:52:24 by tunsal            #+#    #+#             */
-/*   Updated: 2024/06/23 19:38:46 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/06/24 17:15:08 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void	join_threads(t_sim *sim, t_philo *philosophers)
 {
 	int	i;
 
-	pthread_join(sim->observer_thread, NULL);
 	i = 0;
 	while (i < sim->num_of_philos)
 	{
 		pthread_join(philosophers[i].thread, NULL);
 		++i;
 	}
+	pthread_join(sim->observer_thread, NULL);
 }
