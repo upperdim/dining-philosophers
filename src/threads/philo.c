9680680fb53dc5f37a52a52fa58de97830670a48
upperdim <45048352+upperdim@users.noreply.github.com>
2024-06-24 20:03:06 +0200

    prevent 1 philo from picking fork again

--- src/threads/philo.c
+++ src/threads/philo.c
@@ -6,61 +6,33 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 17:01:24 by tunsal            #+#    #+#             */
-/*   Updated: 2024/06/24 19:46:18 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/06/24 20:01:22 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-// int	get_first_fork_idx(t_routine_arg *arg)
-// {
-// 	if (arg->philo->index == arg->sim->num_of_philos - 1)
-// 	{
-// 		return (arg->philo->index);
-// 	}
-// 	else
-// 	{
-// 		return ((arg->philo->index + 1) % arg->fork_count);
-// 	}
-// }
-
-// int	get_second_fork_idx(t_routine_arg *arg)
-// {
-// 	if (arg->philo->index == arg->sim->num_of_philos - 1)
-// 	{
-// 		return ((arg->philo->index + 1) % arg->fork_count);
-// 	}
-// 	else
-// 	{
-// 		return (arg->philo->index);
-// 	}
-// }
-
 static void	philo_eat(t_routine_arg *arg)
 {
 	pthread_mutex_lock(&arg->forks[arg->philo->index]);
-	// pthread_mutex_lock(&arg->forks[get_first_fork_idx(arg)]);
 	print_msg("has taken a fork", arg->sim, arg->philo);
 	if (arg->fork_count == 1)
 	{
+		sleep_ms(arg->sim->time_to_die_wo_eating + 1, arg->sim);
 		pthread_mutex_unlock(&arg->forks[arg->philo->index]);
 		return ;
 	}
 	pthread_mutex_lock(&arg->forks[(arg->philo->index + 1) % arg->fork_count]);
-	// pthread_mutex_lock(&arg->forks[get_second_fork_idx(arg)]);
 	print_msg("has taken a fork", arg->sim, arg->philo);
-	// arg->philo->state = STATE_EATING;
 	set_int(&arg->philo->state_mutex, &arg->philo->state, STATE_EATING);
 	print_msg("is eating", arg->sim, arg->philo);
 	arg->philo->num_times_ate++;
 	if (gettimeofday(&arg->philo->last_eat_timestamp, NULL) == -1)
 		printf("Gettimeofday failed\n");
 	sleep_ms(arg->sim->time_to_eat, arg->sim);
-	pthread_mutex_unlock(&arg->forks[arg->philo->index]);
 	pthread_mutex_unlock(\
 	&arg->forks[(arg->philo->index + 1) % arg->fork_count]);
-	// pthread_mutex_unlock(&arg->forks[get_second_fork_idx(arg)]);
-	// pthread_mutex_unlock(&arg->forks[get_first_fork_idx(arg)]);
+	pthread_mutex_unlock(&arg->forks[arg->philo->index]);
 }
 
 static void	philo_think(t_routine_arg	*arg)
