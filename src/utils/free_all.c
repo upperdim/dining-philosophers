efbb8ba64f0a9973fd8e28fae44aff85bfdfdac2
upperdim <45048352+upperdim@users.noreply.github.com>
2024-06-24 18:00:34 +0200

    freeing and destroying allocations and initializations

--- src/utils/free_all.c
+++ src/utils/free_all.c
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 17:30:27 by tunsal            #+#    #+#             */
-/*   Updated: 2024/06/24 17:43:25 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/06/24 17:56:34 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,13 @@ void	free_all(t_philo *philos, pthread_mutex_t *forks, t_sim *sim)
 	i = 0;
 	while (i < sim->num_of_philos)
 	{
+		pthread_mutex_destroy(&philos[i].mutex);
 		++i;
 	}
-	
+	pthread_mutex_destroy(&sim->sim_mutex);
+	pthread_mutex_destroy(&sim->write_mutex);
+	if (sim->philo_r_args_arr != NULL)
+		free(sim->philo_r_args_arr);
+	if (sim->observer_r_arg != NULL)
+		free(sim->observer_r_arg);
 }
